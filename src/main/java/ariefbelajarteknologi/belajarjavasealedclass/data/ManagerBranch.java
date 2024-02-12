c07ce592bedb19127d85abe44687f63021a9b6a0
Arief-Karditya <nolifegansaya@gmail.com>
2024-02-12 10:15:32 +0700

    Non-Sealed Class

---
+++ src/main/java/ariefbelajarteknologi/belajarjavasealedclass/data/ManagerBranch.java
@@ -0,0 +1,14 @@
+package ariefbelajarteknologi.belajarjavasealedclass.data;
+
+public final class ManagerBranch extends Manager{
+
+    private String branch;
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+}
---
+++ src/main/java/ariefbelajarteknologi/belajarjavasealedclass/data/ManagerOps.java
@@ -0,0 +1,14 @@
+package ariefbelajarteknologi.belajarjavasealedclass.data;
+
+public class ManagerOps extends Manager{
+
+    private String operation;
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+}
